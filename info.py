41b1749f5e88e2860b4f597b9dcc398852ce92d4
moviemasala <118058892+moviemasala@users.noreply.github.com>
2023-01-26 09:54:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS' ,'https://telegra.ph/file/2817d324022fe485880ac.jpg ')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS, '995932132').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '995932132').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001742616778  -1001680841206  -1001614580625  -10034182442').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
